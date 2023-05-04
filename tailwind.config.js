b2f845f362a00a4080986c9de038e2fc64e632aa
humanity1111 <xoliqovmuzaffar240@gmail.com>
2023-05-04 19:25:08 +0500

    tailwind-second-task-landing-page-by-zoltanszognenyi.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      // backgroundImage: {
+      //   `payment-bg-image`: "url:(`./images/phone-mockup.png`)"
+      // }
+    },
+    container: {
+      center: `base`
+    }
+  },
+  plugins: [],
+}
+
